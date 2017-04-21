9f72b861824af0e79c8341e9c3f068bd36a4cffe
Will Price <will.price94@gmail.com>
2017-04-21 18:50:16 +0100

    Uncomment debug predicates in debug.pl

--- debug.pl
+++ debug.pl
@@ -1,14 +1,13 @@
-:- module(debug, []).
-% prolog_trace_interception(Port, Frame, Choice, continue) :-
-%     prolog_frame_attribute(Frame, goal, Goal),
-%     print_debug_info(Port, Frame, Choice).
-% prolog_trace_interception(Port, Frame, Choice, continue).
-%
-% print_debug_info(Port, Frame, Choice) :-
-%     prolog_frame_attribute(Frame, goal, Goal),
-%     format('port: ~p~n', [Port]),
-%     format('goal: ~p~n', [Goal]),
-%     format('frame ~p~n', [Frame]),
-%     format('choice ~p~n', [Choice]).
+prolog_trace_interception(Port, Frame, Choice, continue) :-
+    prolog_frame_attribute(Frame, goal, Goal),
+    print_debug_info(Port, Frame, Choice).
+prolog_trace_interception(Port, Frame, Choice, continue).
+
+print_debug_info(Port, Frame, Choice) :-
+    prolog_frame_attribute(Frame, goal, Goal),
+    format('port: ~p~n', [Port]),
+    format('goal: ~p~n', [Goal]),
+    format('frame ~p~n', [Frame]),
+    format('choice ~p~n', [Choice]).
 
 % vim: set ft=prolog:
