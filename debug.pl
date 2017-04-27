c4908335b61d09fa2321f90755c7d11beebb24fa
Will Price <will.price94@gmail.com>
2017-04-27 12:56:29 +0100

    Remove debug predicates, replace with helper script
    debug.pl no longer contains prolog tracer interception handlers, but is
    just a help script for debugging loading prolog files and setting debug
    topics.

--- debug.pl
+++ debug.pl
@@ -1,13 +1,6 @@
-prolog_trace_interception(Port, Frame, Choice, continue) :-
-    prolog_frame_attribute(Frame, goal, Goal),
-    print_debug_info(Port, Frame, Choice).
-prolog_trace_interception(Port, Frame, Choice, continue).
-
-print_debug_info(Port, Frame, Choice) :-
-    prolog_frame_attribute(Frame, goal, Goal),
-    format('port: ~p~n', [Port]),
-    format('goal: ~p~n', [Goal]),
-    format('frame ~p~n', [Frame]),
-    format('choice ~p~n', [Choice]).
+:- gdebug.
+:- debug.
+:- debug('api-server').
+:- debug('search').
 
 % vim: set ft=prolog:
